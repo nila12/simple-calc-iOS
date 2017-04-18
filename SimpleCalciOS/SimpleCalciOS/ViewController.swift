b87afb042e6ca89880a1ec6521959ebbcb5b64ff
Mathi Manavalan <mathimano@hotmail.com>
2017-04-17 20:14:40 -0700

    started new xcodeproj

---
+++ SimpleCalciOS/SimpleCalciOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalciOS
+//
+//  Created by Mathi Manavalan on 4/17/17.
+//  Copyright © 2017 Mathi Manavalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
